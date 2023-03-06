2a3bc4d31e2ddb03988bb98e61cc33cb377d43e4
Shrey2504 <guptask_2@rknec.edu>
2023-03-06 18:20:39 +0530

    login

--- Home/urls.py
+++ Home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from Home import views
 
 urlpatterns =[
-    path('',views.home)
+    path('',views.home),
+    path('signup/',views.signup),
+    path('login/',views.login),
 ]
--- Home/views.py
+++ Home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'Home/Home.html')
+
+def login(request):
+    return render(request, 'Home/Login.html')
+
+def signup(request):
+    return render(request, 'Home/Signup.html')
