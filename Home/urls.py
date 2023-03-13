ecb1941f64b7aa8742296c95e575bf0a74653767
Shrey2504 <guptask_2@rknec.edu>
2023-03-13 14:59:02 +0530

    livescore

--- Home/urls.py
+++ Home/urls.py
@@ -5,5 +5,6 @@ urlpatterns =[
     path('',views.home, name='home'),
     path('signup/',views.signup, name='signup'),
     path('login/',views.login, name='login'),
-    path('analysis',views.analysis, name='analysis'),
+    path('analysis/',views.analysis, name='analysis'),
+    path('livescore/',views.livescore, name='livescore'),
 ]
