9304cdf4c929bdffca83470be19dc5b99d1bbf19
Shrey2504 <guptask_2@rknec.edu>
2023-04-27 09:14:27 +0530

    commit

--- Home/urls.py
+++ Home/urls.py
@@ -6,5 +6,4 @@ urlpatterns =[
     path('signup/',views.signup, name='signup'),
     path('login/',views.login, name='login'),
     path('analysis/',views.analysis, name='analysis'),
-    path('livescore/',views.livescore, name='livescore'),
 ]
